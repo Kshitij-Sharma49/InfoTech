be37fa4184f02011366b2086384fbd4db3c6902b
Kshitij-Sharma49 <202052321@iiitvadodara.ac.in>
2023-04-27 20:41:10 +0530

    fixes

--- app.py
+++ app.py
@@ -29,4 +29,4 @@ def get_sim_docs(doc_id):
 # api.add_resource(TestClass, "/next")    
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True,host='0.0.0.0')
---
+++ wsgi.py
@@ -0,0 +1,4 @@
+from app import app
+
+if __main__ == "__app__":
+    app.run()
